e868e01bf89ba5a17a77bd26e2903a963b27d4fd
combs <ccombs@gmail.com>
2014-12-17 20:21:16 -0500

    search form, API hit for questions

--- js/stackexchangesearch.js
+++ js/stackexchangesearch.js
@@ -1,9 +1,52 @@
+var data_search = [];
+var data_answers = [];
 
 
 function begin() {
 	
+	$("#form_search").submit(formSearchSubmit);
+		
 	
 }
+
+function fetchSearch($query) {
+	var $url="http://api.stackexchange.com/2.2/search?order=desc&sort=activity&site=stackoverflow";
+	$("#alert_search_blank").hide();
+
+	//  TK: do this in the background
+	 
+	$.getJSON($url, { intitle: $query } ).done(saveSearch);
+	
+}
+
+function saveSearch($data) {
+	$data_search=$data;
+	alert("Got results");
+}
+
+
+function formSearchSubmit() {
+	var $query=$("#input_search_query").val();
+		
+	if ($query=="") {	
+		$("#alert_search_blank").show();
+		return false;
+	} else {
+		
+		
+		fetchSearch($query);	
+		
+		// TK: show status message
+		
+		return false;
+		
+	}
+	
+	
+	return false;	
+	
+}
+
 
 
 
